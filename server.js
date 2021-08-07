ddee3435d33307e432e2991656c8a0c2caa47924
Ahmed Meftah <contact@ahmedmeftah.com>
2021-08-08 00:31:49 +0100

    start decomposing landing page into components

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public/landing', 'index.html'))
+  // res.sendFile(path.join(__dirname, 'public/landing', 'index.html'))
+  res.render('index')
 })
 
 app.get('/login', function (req, res) {
