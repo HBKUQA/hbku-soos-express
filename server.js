a5214078b73fb034ca3cc4b4681be207d3d01733
smida.montasser <Scoop007#>
2021-08-08 12:00:46 +0200

    fix new comments

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', function (req, res) {
+app.get('/home', function (req, res) {
   // res.sendFile(path.join(__dirname, 'public/landing', 'index.html'))
   res.render('index')
 })
